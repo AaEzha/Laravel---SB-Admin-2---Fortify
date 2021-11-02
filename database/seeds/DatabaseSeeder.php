07ffae3b0fdc63862357abcc8cfab08d7ae6184b
Aa Ezha <aaezha@gmail.com>
2021-11-02 12:37:13 +0700

    Add: 1 user seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\User;
+use Database\Seeders\AdminUserSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        User::create([
+            'name' => 'Super',
+            'last_name' => 'Administrator',
+            'email' => 'admin@mail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        ]);
     }
 }
