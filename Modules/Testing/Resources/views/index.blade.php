d57b474542494acf191d605b228b17cb78cbee15
AaEzha <aaezha@gmail.com>
2023-06-27 14:22:35 +0700

    remove nwidart/laravel-modules ❌

--- Modules/Testing/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('testing::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('testing.name') !!}
-    </p>
-@endsection
